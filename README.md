aa61bbdf5eae12b2f7ea44f80836ee832184e7d8
Tower He <towerhe@gmail.com>
2012-04-13 17:20:47 +0800

    Initial import.

---
+++ README.md
@@ -0,0 +1,23 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* rails_basics
+* git
+* jquery
+* rspec
+* simple_form
+* sqlite3
+* twitter_bootstrap
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberCartExample::Application.config.secret_token = 'cbfa639342acc7a7b69a71c612f20805e725628de400baec8e0d243f0dd89951a6dd8864ce8f93b458933832d5500ffbcd379bd5750ff13342bcda2f1f631f5f'
