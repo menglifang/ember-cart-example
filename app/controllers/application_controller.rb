fce2813daac417e6f9af29f4ab4d13012179adf7
Tower He <towerhe@gmail.com>
2012-04-16 14:36:23 +0800

    Set up ember-cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_or_create_carts
+
+  helper_method :ember_carts, :current_cart
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EmberCartExample::Application.routes.draw do
+  mount EmberCart::Engine => "ember_cart", as: :ember_cart
+
   root :to => 'product_categories#index'
 
   resources :product_categories do
