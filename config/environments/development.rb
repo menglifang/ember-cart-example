1ae9d035beb6da8e789e3437a516ce98b7aca714
Tower He <towerhe@gmail.com>
2012-08-12 12:15:12 +0800

    Config ember

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ EmberCartExample::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.handlebars.precompile = false
+  config.ember.variant = :development
 end
