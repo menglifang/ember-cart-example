becadad4cf3f6d207aa235efd8183eefeb189db3
Tower He <towerhe@gmail.com>
2012-08-12 12:17:44 +0800

    Separate ember-cart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,3 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-//
-//= require ember_cart/application
---
+++ app/assets/javascripts/ember-cart.js.coffee
@@ -0,0 +1,5 @@
+#= require ember_cart/application
+#= require_self
+
+#Ember.StateManager.reopen
+  #enableLogging: true
